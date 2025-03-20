2806d1fa2ad7b5ea82f9d71f4f2e638eacaf52bc
luis <gutierrez.luis@icloud.com>
2025-03-20 15:26:44 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from phrasehunter.game import Game
-# from phrasehunter.phrase import Phrase
+from phrasehunter.phrase import Phrase
 
 
 if __name__ == "__main__":
 #    phrase = Phrase()
 # Inside Dunder Main:
-# Create an instance of your Game class
-    game = Game()
-
-    for phrase in game.phrases:
-        print(phrase.phrase)
+#    print(phrase)
+ #   print(game)
 
 # Start your game by calling the instance method that starts the game loop
 # create_instance.start()
+    game = Game()
+#    print(game.active_phrase.phrase) # removed in step 10
+    game.start()
+
+
