b078a0efba0bcc55148b12f70e745507c821225e
luis <gutierrez.luis@icloud.com>
2025-03-20 15:10:13 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,26 +3,15 @@ from phrasehunter.phrase import Phrase
 
 
 if __name__ == "__main__":
-    phrase = Phrase()
+#    phrase = Phrase()
 # Inside Dunder Main:
-# Create an instance of your Game class
-    game = Game()
 #    print(phrase)
  #   print(game)
 
-
-    for phrase in game.phrases:
-        print(phrase.phrase)
-
-
 # Start your game by calling the instance method that starts the game loop
 # create_instance.start()
-#    def print_phrase(phrase_object):
-#        print(f"The phrase is: {phrase_object.phrase}")
+    game = Game()
+#    print(game.active_phrase.phrase) # removed in step 10
+    game.start()
 
-#    game = Game()
-#    print_phrase(game.get_random_phrase())
-#    print_phrase(game.get_random_phrase())
-#    print_phrase(game.get_random_phrase())
-#    print_phrase(game.get_random_phrase())
-#    print_phrase(game.get_random_phrase())
+
